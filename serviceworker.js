dd9194a548a34a398f88e32d133bf04268eaf150
Satish Ramteare <satishramteare@Zendi13.zoomtown.com>
2019-12-05 12:20:54 -0500

    updated view code with animations. Updated Read me. Updated cache update when online.

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,6 @@
 "use strict";
-const APP_SHELL_CACHE_NAME = "static-cache-v2";
-const DYNAMIC_RES_CAHCE_NAME = "dynamic-cache-v1";
+const APP_SHELL_CACHE_NAME = "static-cache-v0.2";
+const DYNAMIC_RES_CAHCE_NAME = "dynamic-cache-v.0q2";
 const appShellResourceList = ["/index.html", "/app.css"];
 
 self.addEventListener("install", event => {
@@ -34,28 +34,35 @@ self.addEventListener("fetch", evt => {
   evt.respondWith(
     caches.match(evt.request).then(resp => {
       if (resp) {
+        if (navigator.onLine) {
+          doFetch(evt);
+        }
+        console.log("[ServiceWorker] respondind from cache", evt.request.url);
         return resp;
       }
-      return fetch(evt.request).then(resp => {
-        if (!resp || resp.status !== 200) {
-          return resp;
-        }
-        let respClone = resp.clone();
-        caches
-          .open(DYNAMIC_RES_CAHCE_NAME)
-          .then(cache => cache.put(evt.request, respClone));
-        return resp;
-      });
+      return doFetch(evt);
     })
   );
 });
 
 function doFetch(evt) {
-  fetch(evt.request).then(function(response) {
+  /*fetch(evt.request).then(function(response) {
     return caches.open(DYNAMIC_RES_CAHCE_NAME).then(function(cache) {
       console.log("[Service Worker] Caching new resource: " + evt.request.url);
       cache.put(evt.request, response.clone());
       return response;
     });
+  });*/
+
+  return fetch(evt.request).then(resp => {
+    if (!resp || resp.status !== 200) {
+      return resp;
+    }
+    let respClone = resp.clone();
+    caches.open(DYNAMIC_RES_CAHCE_NAME).then(cache => {
+      console.log("[Service Worker] Caching new resource: " + evt.request.url);
+      cache.put(evt.request, respClone);
+    });
+    return resp;
   });
 }
